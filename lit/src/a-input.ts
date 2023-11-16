9b9d91532e9a2ad617aff18091c54d2395c13361
Ken <ken.aleksander@gmail.com>
2023-11-16 13:51:19 +0100

    :art: Input with formAssociated

--- lit/src/a-input.ts
+++ lit/src/a-input.ts
@@ -1,5 +1,6 @@
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
+/* import { FormControlController } from "./internals/FormController"; */
 
 /**
  * An example element.
@@ -9,6 +10,29 @@ import { customElement, property } from "lit/decorators.js";
  */
 @customElement("a-input")
 export class AInput extends LitElement {
+  /* private readonly formControlController = new FormControlController(this, {
+    assumeInteractionOn: ["a-input"],
+  }); */
+
+  static formAssociated = true;
+  private _internals: ElementInternals;
+  value = "";
+
+  constructor() {
+    super();
+    this._internals = this.attachInternals();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._internals.setFormValue(this.value);
+  }
+
+  private handleInput(e: any) {
+    this.value = e.target.value;
+    this._internals.setFormValue(this.value);
+  }
+
   @property()
   label = "Placeholder label";
 
@@ -16,7 +40,7 @@ export class AInput extends LitElement {
     return html`
       <div class="form-wrapper">
         <label>${this.label}</label>
-        <input type="text" />
+        <input type="text" @change=${this.handleInput} />
       </div>
     `;
   }
